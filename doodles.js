e95fc465f8a1224c66b64fd87c11c12c9c691cb9
Stephen Young <StephenJayYoung@gmail.com>
2014-10-25 09:55:30 -0700

    added some stuff to css

---
+++ doodles.js
@@ -0,0 +1,22 @@
+
+
+
+// var sayNumber = function(n) {
+//   console.log("Stephen says '" + n.toString() + "'");
+// };
+
+
+Var person = {
+	firstname: "Steve",
+	lastname: 'Young',
+	Age: 27
+};
+
+
+
+if (person[0] === 'Steve') {
+	console.log ('he is  a prettycool dude');
+
+	else if (name === "Mike")
+	console.log ('Oh, he's cool too. Talk to him after Steve');
+};
